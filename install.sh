5d6f3f9fa12f9f888206c541c8e26adeeea476cc
wildheart <boxbit@gmail.com>
2019-07-06 15:46:14 -0400

    Update install.sh
    Added check for api level

--- install.sh
+++ install.sh
@@ -9,22 +9,14 @@ print_modname() {
   ui_print "     App Ops System Plugin     "
   ui_print "*******************************"
 }
-
-# Copy/extract your module files into $MODPATH in on_install.
-
 on_install() {
   # if api 28 or greater abort install
-  if [$API > 25]; then
-  abort => "Does not work for API 28 or greater"
+  if [ $API -gt 27 ]; then
+  abort "App Ops System Plugin not work for API 28 or greater"
   fi
   ui_print "- Extracting module files"
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 }
-
-# Only some special files require specific permissions
-# This function will be called after on_install is done
-# The default permissions should be good enough for most cases
-
 set_permissions() {
   # The following is the default rule, DO NOT remove
   set_perm_recursive $MODPATH 0 0 0755 0644
@@ -35,5 +27,3 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/dex2oat         0     2000    0755      u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0     0       0644
 }
-
-# You can add more functions to assist your custom script code
